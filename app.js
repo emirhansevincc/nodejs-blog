796e1cff0ca17d5f010a315df3b16a55d7931e44
Emirhan Sevinc <emirhansvncc@gmail.com>
2022-10-18 16:29:56 +0300

    Showing photos from db

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require("express")
-const ejs = require("ejs")
+const express = require('express');
+const ejs = require('ejs');
 const mongoose = require('mongoose');
-const Photo = require('./models/Photo')
+const Photo = require('./models/Photo');
 
-const app = express()
+const app = express();
 
 mongoose.connect('mongodb://localhost/pcat-test-db', {
   useNewUrlParser: true,
@@ -11,33 +11,33 @@ mongoose.connect('mongodb://localhost/pcat-test-db', {
 });
 
 //Template Engine
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 // Middlewares
-app.use(express.static('public')) // We keep static files in the public folder
-app.use(express.urlencoded({extended:true}))    // for req.body
-app.use(express.json())                         // for req.body
+app.use(express.static('public')); // We keep static files in the public folder
+app.use(express.urlencoded({ extended: true })); // for req.body
+app.use(express.json()); // for req.body
 
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index.ejs', { photos: photos });
+});
 
-app.get("/",(req, res) => {
-    res.render("index.ejs")
-})
+app.get('/about', (req, res) => {
+  res.render('about.ejs');
+});
 
-app.get("/about",(req, res) => {
-    res.render("about.ejs")
-})
+app.get('/add', (req, res) => {
+  res.render('add.ejs');
+});
 
-app.get("/add",(req, res) => {
-    res.render("add.ejs")
-})
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
+});
 
-app.post('/photos', async(req, res) => {
-    await Photo.create(req.body)
-    res.redirect('/')
-})
-
-const port = 3000
+const port = 3000;
 
 app.listen(port, () => {
-    console.log('Server is up');
-})
+  console.log('Server is up');
+});
